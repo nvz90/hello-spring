f6ac3d79701f6a4b23a50bf56ccd48d2f52ae981
daeyounggu <ocstar9@gmail.com>
2022-11-15 21:30:46 +0900

    스프링 빈 자동등록 @Repository

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();   // 동시성 문제로 인해 실무에서는 concurrenthashmap 씀
