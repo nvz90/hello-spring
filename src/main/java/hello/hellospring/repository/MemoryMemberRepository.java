a750acbfa7421b4e80dff82020c9f7823737f4cd
daeyounggu <ocstar9@gmail.com>
2022-11-16 17:03:21 +0900

    컴포넌트 어노테이션 제거

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
-@Repository
 public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();   // 동시성 문제로 인해 실무에서는 concurrenthashmap 씀
